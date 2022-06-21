024734691444fcb8e1ce8ca7d02b0c244fdbacd3
Estanley Santana <contatostanmilke@gmail.com>
2022-06-21 17:24:40 -0300

    sidebar finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 import { gql, useQuery } from "@apollo/client"
+import { Hearder } from "./components/Header"
+import { Lesson } from "./components/Lesson"
+import { Sidebar } from "./components/Sidebar"
+import { Video } from "./components/Video"
+import { Event } from "./pages/Event"
 
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
-interface Lesson {
-  id: string;
-  title: string;
-}
 
 function App() {
-  const { data } = useQuery<{ lessons: Lesson }>(GET_LESSONS_QUERY) 
+  
 
   return (
-    <h1></h1>
+    <div>
+      <Event />
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,36 @@ module.exports = {
     './src/**/*.tsx'
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: 'Roboto, sans-serif'
+      },
+      colors: {
+        yellow: {
+          300: '#fbbf24',
+          500: '#f59e0b',
+          700: '#d97706',
+        },
+        blue: {
+          500: '#81D8F7',
+        },
+        orange: {
+          500: '#FBA94C',
+        },
+        red: {
+          500: '#F75A68',
+        },
+        gray: {
+          100: '#E1E1E6',
+          200: '#C4C4CC',
+          300: '#8D8D99',
+          500: '#323238',
+          600: '#29292E',
+          700: '#121214',
+          900: '#09090A'
+        }
+      },
+    },
   },
   plugins: [],
 }
